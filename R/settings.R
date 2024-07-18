169b067d7932d291e372e91817506e448fbd12da
danielparthier <daniel.parthier@gmail.com>
2024-07-18 11:36:27 +0200

    initiate settings

---
+++ R/settings.R
@@ -0,0 +1,4 @@
+project_path <- "/user/Vogt-et-al-2024/" # path of project
+n_cores <- 10 # number of cores to use for parallel processing
+n_chains <- 5 # number of parallel chains (each on one CPU thread) for model
+n_samples <- 20000 # number of samples to draw from posterior distribution
