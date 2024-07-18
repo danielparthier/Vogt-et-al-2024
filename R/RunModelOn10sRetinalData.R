6f7bc4b7c84482cabc7f63e0b54ec1bfa8bbf574
danielparthier <daniel.parthier@gmail.com>
2024-07-18 11:37:17 +0200

    files to run model

---
+++ R/RunModelOn10sRetinalData.R
@@ -0,0 +1,29 @@
+setwd(dir = projectDir)
+source("R/LoadDataFunctions.R")
+
+n_samples_per_chain <- ceiling(n_samples/n_chains)
+
+AssayList <- LoadData("jellyRetinalsOnly", illumination_range = c(10000, 10000))
+stanData <- ConstructStanData(AssayList)
+
+dirPath <- "Output/20240611_10sRetinals/"
+if(!dir.exists(paths = dirPath)) {
+  dir.create(path = dirPath)
+}
+
+ModelRun(stanData = stanData,
+         n_chains = n_chains,
+         n_samples = n_samples_per_chain,
+         output_path = dirPath,
+         prior_only = F)
+
+stanDataPrior <- stanData
+stanDataPrior$priorOnly <- 1
+
+ModelRun(stanData = stanData,
+         n_chains = n_chains,
+         n_samples = n_samples_per_chain,
+         output_path = dirPath,
+         prior_only = T)
+
+saveRDS(object = AssayList, file = paste0(dirPath, "AssayList.rds"))
